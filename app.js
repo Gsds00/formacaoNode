c078f57dedf97144b11049f91a7ba4d08192a1a3
Gabriel Severino <gabrielseverinooo000@gmail.com>
2022-12-02 13:30:19 -0300

    metodo get  funcionando

---
+++ app.js
@@ -0,0 +1,23 @@
+//REquiring module
+const express = require('express');
+
+//Creating express object
+const app = express();
+
+////Handling GET request
+app.get('/', (req, res) => {
+    res.send('A api esta funcionando '+ 'neste servidor ')
+    res.end()
+})
+
+////Handling GET request
+app.get('/retorno', (req, res) => {
+    res.send('Nodejs é facil. ')
+    res.end()
+})
+    
+//port Number
+const PORT = process.env.PORT ||5000;
+
+//Server setup
+app.listen(PORT,console.log(`Server started on port${PORT}`));
